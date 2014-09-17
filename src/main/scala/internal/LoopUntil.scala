f59f2ae5cf24f01d94dcfdcbce91b19083f54080
Sisi Cheng <scheng@hmc.edu>
2014-09-17 15:25:48 -0700

    Half way done with in-class lab.

--- src/main/scala/internal/LoopUntil.scala
+++ src/main/scala/internal/LoopUntil.scala
@@ -12,8 +12,14 @@ object LoopUntil extends App {
 
   // define the new control-flow structure here
 
-  var i = 0
+  def loop_until(condition: => Boolean)(body: => Unit): Unit = {
+    if (!condition) {
+      body
+      loop_until(condition)(body)
+    }
+  }
   
+  var i = 0
   loop_until (i > 9) {
       if ( (i % 2) == 0 )
           println(i)
--- src/main/scala/internal/RepeatUntil.scala
+++ src/main/scala/internal/RepeatUntil.scala
@@ -9,10 +9,32 @@ package internal
  * 8
  */
 
+/*Sadly broken atm: printing out "0" then going into an infinite loop until it runs out of memory*/
+
 object RepeatUntil extends App {
   
+  // companion class for repeat.
+  object repeat {
+	def apply(x: => Unit):repeat = {
+		new repeat {
+			var body:Unit = x
+		}
+	}
+  }
+  
   // define the new control-flow structure here
-
+  abstract class repeat {
+    var body:Unit
+	
+    def until(condition: => Boolean): Unit = {
+      if (!condition) {
+    	body // warning on this line: "a pure expression does nothing in a statement position
+        repeat(body) until(condition)
+      }
+    }
+  }
+ 
+  
   var i = 0
   repeat  {
       if ( (i % 2) == 0 )
